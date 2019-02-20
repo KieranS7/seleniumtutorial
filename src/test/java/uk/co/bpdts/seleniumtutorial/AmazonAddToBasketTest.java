7b5b9ebfd26c089760624cb06c0075b75c3c5c68
Kieran Slater <kieran_slater@hotmail.co.uk>
2019-02-20 10:05:49 +0000

    adding first failing test

---
+++ src/test/java/uk/co/bpdts/seleniumtutorial/AmazonAddToBasketTest.java
@@ -0,0 +1,18 @@
+package uk.co.bpdts.seleniumtutorial;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class AmazonAddToBasketTest {
+
+    @Test
+    public void testSearchForProductAndAddToBasket() {
+
+        fail("first test failure");
+
+
+    }
+
+}
+
