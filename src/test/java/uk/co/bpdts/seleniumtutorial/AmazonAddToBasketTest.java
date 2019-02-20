372df47c464ae413f5e0cfde2ab49918eb027f0d
Kieran Slater <kieran_slater@hotmail.co.uk>
2019-02-20 10:54:08 +0000

    adding first test to go to amazon and check URL in chrome

--- src/test/java/uk/co/bpdts/seleniumtutorial/AmazonAddToBasketTest.java
+++ src/test/java/uk/co/bpdts/seleniumtutorial/AmazonAddToBasketTest.java
@@ -1,17 +1,34 @@
 package uk.co.bpdts.seleniumtutorial;
 
+import org.hamcrest.MatcherAssert;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class AmazonAddToBasketTest {
 
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver",this.getClass().getClassLoader().getResource("chromedriver").getPath());
+        driver = new ChromeDriver();
+    }
+
     @Test
     public void testSearchForProductAndAddToBasket() {
+        driver.get("https://www.amazon.co.uk");
+        assertThat("URL is wrong",driver.getCurrentUrl(),is("https://www.amazon.co.uk/"));
+    }
 
-        fail("first test failure");
-
-
+    @After
+    public void tearDown() {
+        driver.quit();
     }
 
 }
